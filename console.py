e85324b203a0a625f136cc404285ac0687b06baf
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-02-11 08:11:05 +0200

    console.py

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """command interpreter."""
 import cmd
+import json
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
